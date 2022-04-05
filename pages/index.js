33b7131fb0d38e8590a59eef61540e403e8ae5c1
kmi <kkmo>
2022-04-05 14:37:29 +0200

    clean slate

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,5 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <>
-      <div className={styles.container}>
-        <h1>Employee App</h1>
-        <ul className={styles.list}>
-          <li>Make a custom hook for fetching the data</li>
-          <li>Create React Hook Form</li>
-          <li>Use React Query Mutation to update data</li>
-          <li>Delete Data</li>
-        </ul>
-      </div>
-    </>
-  );
+  return <></>;
 }
